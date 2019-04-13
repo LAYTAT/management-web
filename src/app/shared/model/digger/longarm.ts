ef2d5fe3292abc682aa9817c6071d8952531a2cd
WIS <38274782+WIS8@users.noreply.github.com>
2019-04-13 21:28:39 +0800

    modify flares

--- src/app/shared/model/digger/longarm.ts
+++ src/app/shared/model/digger/longarm.ts
@@ -22,6 +22,8 @@ import {ArmsModule} from './arms';
 
 /* 长臂模块类 */
 export class LongArm extends ArmsModule {
+  // 照明灯
+  private flare: Object3D;
   // 灯光物体
   private lightL: SpotLight;
   private lightR: SpotLight;
@@ -39,14 +41,14 @@ export class LongArm extends ArmsModule {
   // 开关灯方法
   public lightsOn(operate: boolean): void {
     if (operate) {
-      this.model.add(this.lightL);
-      this.model.add(this.lightR);
+      this.flare.add(this.lightL);
+      this.flare.add(this.lightR);
     } else {
-      if (this.model.getObjectByName('left-light')) {
-        this.model.remove(this.lightL);
+      if (this.flare.getObjectByName('left-light')) {
+        this.flare.remove(this.lightL);
       }
-      if (this.model.getObjectByName('right-light')) {
-        this.model.remove(this.lightR);
+      if (this.flare.getObjectByName('right-light')) {
+        this.flare.remove(this.lightR);
       }
     }
   }
@@ -58,9 +60,9 @@ export class LongArm extends ArmsModule {
     const mainLength = mainRadius * 1.4;
     const axisRadius = mainRadius * 0.2;
     const axisHeight = mainLength * 2;
-    const flareRadius = mainRadius * 0.2;
-    const flareWidth = flareRadius * 0.3;
-    const flareLength = mainLength * 0.6;
+    const flareRadius = mainRadius * 0.4;
+    const flareHeight = mainRadius * 0.8;
+    const flareLength = flareHeight * 0.5;
 
     // 定义长臂的形体和材质纹理
     const geometry = new Geometry();
@@ -88,27 +90,44 @@ export class LongArm extends ArmsModule {
     main.translate(0, 0, -mainLength / 2);
     geometry.merge(main);
 
-    // 添加照明灯
-    extrude.depth = flareLength;
-    const flareR = new ExtrudeGeometry(this.getFlareShape(flareRadius, flareWidth), extrude);
-    flareR.translate(7 * mainRadius, 4 * mainRadius, mainLength / 2);
-    geometry.merge(flareR);
+    // 定义照明灯的形体和材质纹理
+    const flareGeometry = new Geometry();
+    const flareMaterial = new MeshPhongMaterial({shininess: 100});
+    flareMaterial.map = (new TextureLoader()).load('./assets/textures/flare.jpg',
+      function (texture: Texture): void {
+        flareMaterial.map = texture;
+      });
+
+    // 添加左右照明灯
+    const flareR = new CylinderGeometry(flareRadius / 2, flareRadius, flareLength,
+      20, 1, false);
+    flareR.translate(0, (flareLength + flareHeight) / 2, 0);
+    flareR.merge(new CylinderGeometry(flareRadius, flareRadius, flareHeight,
+      20, 1, false));
+    flareR.translate(0, flareHeight / 2, 0);
     const flareL = flareR.clone();
-    flareL.translate(0, 0, -mainLength - flareLength);
-    geometry.merge(flareL);
+    flareL.translate(0, 0, -mainLength / 2 - flareRadius);
+    flareR.translate(0, 0, mainLength / 2 + flareRadius);
+    flareGeometry.merge(flareL);
+    flareGeometry.merge(flareR);
+    flareGeometry.rotateZ(Math.PI / 2);
 
     // 合成物体
     this.model = new Object3D();
     this.model.add(Common.createMesh(geometry, material));
     this.model.position.set(1.5 * mainRadius, 1.5 * mainRadius, 0.375 * mainRadius);
+    this.flare = new Object3D();
+    this.flare.add(Common.createMesh(flareGeometry, flareMaterial));
+    this.flare.position.set(7 * mainRadius, 4 * mainRadius, 0);
+    this.model.add(this.flare);
 
     // 光照定向的虚拟物体
     const glassL = Common.createMesh(new Geometry(), new MeshBasicMaterial(),
-      7 * mainRadius + flareWidth, 4 * mainRadius, (-mainLength - flareLength) / 2);
-    this.model.add(glassL);
+      1, 0, -mainLength / 2 - flareRadius);
+    this.flare.add(glassL);
     const glassR = glassL.clone();
-    glassR.translateZ(mainLength + flareLength);
-    this.model.add(glassR);
+    glassR.translateZ(mainLength + 2 * flareRadius);
+    this.flare.add(glassR);
 
     // 初始化灯光
     this.lightL = new SpotLight(0xffaa55);
@@ -116,11 +135,11 @@ export class LongArm extends ArmsModule {
     this.lightL.target = glassR;
     this.lightL.castShadow = true;
     this.lightL.exponent = 1;
-    this.lightL.position.set(7 * mainRadius, 4 * mainRadius, (-mainLength - flareLength) / 2);
+    this.lightL.position.set(0, 0, -mainLength / 2 - flareRadius);
     this.lightR = this.lightL.clone();
     this.lightR.name = 'right-light';
     this.lightR.target = glassL;
-    this.lightL.translateZ(mainLength + flareLength);
+    this.lightL.translateZ(mainLength + 2 * flareRadius);
   }
 
   // 获取长臂的形状
@@ -137,19 +156,6 @@ export class LongArm extends ArmsModule {
     shape.lineTo(13 * r, 3.8 * r);
     shape.lineTo(10 * r, 3.4 * r);
     shape.bezierCurveTo(8 * r, 3.2 * r, 6 * r, 2.5 * r, 3 * r, r);
-    return shape;
-  }
-
-  // 获取照明灯的形状
-  private getFlareShape(r: number, l: number): Shape {
-    const shape = new Shape();
-    shape.moveTo(0, 0);
-    shape.arc(0, 0, r, Math.PI / 2, 1.5 * Math.PI, false);
-    shape.lineTo(0, -r - l);
-    shape.lineTo(l, -r - l);
-    shape.lineTo(l, r + l);
-    shape.lineTo(0, r + l);
-    shape.lineTo(0, r);
     return shape;
   }
 
